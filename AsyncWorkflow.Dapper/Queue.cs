0dca24591b7ff52655f3a02e5a97f51782792469
Adam O'Neil <adamosoftware@gmail.com>
2024-06-13 07:31:59 -0400

    Dapper implementation started

---
+++ AsyncWorkflow.Dapper/Queue.cs
@@ -0,0 +1,34 @@
+﻿using AsyncWorkflow.Dapper.Extensions;
+using AsyncWorkflow.Interfaces;
+using AsyncWorkflow.Records;
+using Microsoft.Extensions.Options;
+using System.Data;
+
+namespace AsyncWorkflow.Dapper;
+
+public class Queue(Func<IDbConnection> connectionFactory, IOptions<QueueSqlOptions> options) : IQueue
+{
+	private readonly Func<IDbConnection> _connectionFactory = connectionFactory;
+	private readonly QueueSqlOptions _options = options.Value;
+
+	public async Task<Message?> DequeueAsync(string machineName, string handler, CancellationToken cancellationToken)
+	{
+		using var cn = _connectionFactory.Invoke();
+
+		var (result, _) = await cn.DequeueAsync<Message>(
+			_options.QueueTable, _options.QueueTableCriteria, 
+			new { machineName, handler });
+
+		return result;
+	}
+
+	public Task<string> EnqueueAsync(string machineName, Message message)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task LogFailureAsync(string machineName, Message message, Exception exception, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ AsyncWorkflow.Dapper/QueueSqlOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace AsyncWorkflow.Dapper;
+
+public class QueueSqlOptions
+{
+	public string QueueTable { get; set; } = default!;
+	/// <summary>
+	/// shouold be a WHERE clause that includes the machineName and handler
+	/// </summary>
+	public string QueueTableCriteria { get; set; } = default!;
+	public string LogTable { get; set; } = default!;
+}
