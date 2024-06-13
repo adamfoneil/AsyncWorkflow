a9b311792e9dd8e951fbf2afba7f593b60122006
Adam O'Neil <adamosoftware@gmail.com>
2024-06-13 07:42:11 -0400

    no need to return a Message.Id when enqueing

--- AsyncWorkflow/Interfaces/IQueue.cs
+++ AsyncWorkflow/Interfaces/IQueue.cs
@@ -4,7 +4,7 @@ namespace AsyncWorkflow.Interfaces;
 
 public interface IQueue
 {
-    Task<string> EnqueueAsync(string machineName, Message message);
+    Task EnqueueAsync(string machineName, Message message);
     Task<Message?> DequeueAsync(string machineName, string handler, CancellationToken cancellationToken);
     Task LogFailureAsync(string machineName, Message message, Exception exception, CancellationToken cancellationToken);
 }
